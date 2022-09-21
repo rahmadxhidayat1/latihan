e76b66fa0683c4fab6e247138ea9a5d58827334d
rahmadxhidayat1 <92037828+rahmadxhidayat1@users.noreply.github.com>
2022-09-21 18:52:05 +0700

    anjay

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,20 @@
+<div class="container">
+    <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
+        <a href="index.html" class="navbar-brand">
+            <h1 class="m-0 text-uppercase text-primary"><i class="fa fa-clinic-medical me-2"></i>Medinova</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto py-0">
+                <a href="{{route('home')}}" class="nav-item nav-link">Home</a>
+                <a class="nav-item nav-link" href="{{route('about')}}">About</a>
+                <a href="{{route('service')}}" class="nav-item nav-link">Service</a>
+                <a href="{{route('price')}}" class="nav-item nav-link">Pricing</a>
+                <a href="{{route('contact')}}" class="nav-item nav-link">Contact</a>
+                <a href="student" class="nav-item nav-link">Latihan</a>
+            </div>
+        </div>
+    </nav>
+</div>
